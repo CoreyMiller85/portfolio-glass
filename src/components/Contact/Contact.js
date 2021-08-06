c3411c67d28fdf716976a280e1e4f1203d20455c
Corey Miller <coreymiller454@gmail.com>
2021-08-06 01:13:45 -0400

    add responsive layout

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,33 @@
-import { Container } from "postcss";
 import React from "react";
 import Section from "../Section/Section";
 const Contact = () => {
-	return <Section title="Contact Me"></Section>;
+	return (
+		<Section title="Contact Me">
+			<form>
+				<label htmlFor="name">
+					Name:
+					<input type="text" name="name" />
+				</label>
+				<label htmlFor="email">
+					Email:
+					<input type="email" name="email" />
+				</label>
+				<label htmlFor="message">
+					Message:
+					<textarea
+						rows="5"
+						id="message-box"
+						name="message-box"
+						class="messageBox"
+						placeholder="Type your message here"
+						pattern="^[a-zA-Z1-9].*"
+						required=""
+					></textarea>
+				</label>
+				<button>Send</button>
+			</form>
+		</Section>
+	);
 };
 
 export default Contact;
--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -10,7 +10,17 @@ function Content() {
 		<div className="content">
 			<HeroImage />
 			<Section title="About Me">
-				<Container></Container>
+				<Container>
+					<p>
+						I am Corey Miller, a Charlotte NC fullstack web developer. I have
+						been working with HTML, CSS, and JavaScript since 2018 creating full
+						stack web apps. I have completed the “Full-Stack Tech Degree” from
+						Team Treehouse which is a rigorous bootcamp focusing on the MERN
+						stack. I am an avid learner and enjoy learning new ways to code and
+						solve problems on a daily basis. I am very interested in working on
+						a team in the greater Charlotte area full time as a web developer.
+					</p>
+				</Container>
 			</Section>
 			<Section title="Projects">
 				<Container>
