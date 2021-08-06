dad57dc400190bcb5ec76df5882b69413f65c538
Corey Miller <coreymiller454@gmail.com>
2021-08-06 14:25:36 -0400

    style contact form, increase font and line height

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,30 +1,31 @@
 import React from "react";
 import Section from "../Section/Section";
+import "./_Contact.scss";
 const Contact = () => {
 	return (
 		<Section title="Contact Me">
-			<form>
-				<label htmlFor="name">
+			<form className="contact__form">
+				<label htmlFor="name" className="contact__name-label">
 					Name:
-					<input type="text" name="name" />
+					<input type="text" name="name" className="contact__name-input" />
 				</label>
-				<label htmlFor="email">
+				<label htmlFor="email" className="contact__email-label">
 					Email:
-					<input type="email" name="email" />
+					<input type="email" name="email" className="contact__email-input" />
 				</label>
-				<label htmlFor="message">
+				<label htmlFor="message" className="contact__message-label">
 					Message:
 					<textarea
 						rows="5"
 						id="message-box"
 						name="message-box"
-						class="messageBox"
+						className="contact__message-textarea"
 						placeholder="Type your message here"
 						pattern="^[a-zA-Z1-9].*"
 						required=""
 					></textarea>
 				</label>
-				<button>Send</button>
+				<button className="contact__button">Send</button>
 			</form>
 		</Section>
 	);
