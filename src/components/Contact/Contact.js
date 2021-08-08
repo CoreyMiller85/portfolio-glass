a954a561817bbdf6d7c652990404f1ba931f3030
Corey Miller <coreymiller454@gmail.com>
2021-08-08 01:09:25 -0400

    hook up email server mailing

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,34 @@
 import React from "react";
 import Section from "../Section/Section";
+import emailjs from "emailjs-com";
+
 import "./_Contact.scss";
+
 const Contact = () => {
+	function sendEmail(e) {
+		e.preventDefault();
+
+		emailjs
+			.sendForm(
+				"service_rxcv3wi",
+				"template_p8onx9r",
+				e.target,
+				"user_N96NhZlNAe8XTpWbD5BCs"
+			)
+			.then(
+				(result) => {
+					console.log(result.text);
+				},
+				(error) => {
+					console.log(error.text);
+				}
+			);
+		e.target.reset();
+	}
+
 	return (
 		<Section title="Contact Me">
-			<form className="contact__form">
+			<form className="contact__form" onSubmit={sendEmail}>
 				<label htmlFor="name" className="contact__name-label">
 					Name:
 					<input type="text" name="name" className="contact__name-input" />
